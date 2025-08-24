6719f9a11f272c38b43ef81f30f5dde43d57b56b
ShangWu <shanw13@uci.edu>
2025-08-23 21:01:31 -0700

    temp comment out bonus message

--- src/feedback.js
+++ src/feedback.js
@@ -135,8 +135,8 @@ async function submitFeedbackForm(submitButton) {
         const phase3Bonus = getBonusAmount(PHASE_NAME.PHASE3);
 
         if (totalBonus > 0) {
-            bonusMessageContent = `Your total bonus is $${totalBonus},
-       which breaks down to $${phase1_2Bonus} for ${getGroupType()=== GROUP_TYPE.EXP_GROUP_THREE ? 'phase1' : 'phase 1 and 2'}, $${phase3Bonus} for phase 3.`;
+    //         bonusMessageContent = `Your total bonus is $${totalBonus},
+    //    which breaks down to $${phase1_2Bonus} for ${getGroupType()=== GROUP_TYPE.EXP_GROUP_THREE ? 'phase1' : 'phase 1 and 2'}, $${phase3Bonus} for phase 3.`;
             thankYouMessageContent = `Thank you for participating in the game! 
 Your bonus payment will be distributed shortly.
 Now you will be redirected back to Prolific.`;
